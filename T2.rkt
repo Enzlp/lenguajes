77b30de760f5859eb15dce7f6c71ddf9d95400e2
Enzo <enzo.lpez.pstor@gmail.com>
2024-09-22 21:57:48 -0300

    avance

--- T2.rkt
+++ T2.rkt
@@ -1,8 +1,13 @@
 #lang play
 
+#| 
+NOMBRE Y APELLIDO: ENZO LOPEZ
+RUT: 20445626-7
+|#
+
 #|
 
-Hizo Ud uso de la whiteboard policy: (Indique SI/NO)
+Hizo Ud uso de la whiteboard policy: NO
 En caso que afirmativo, indique con quién y sobre qué ejercicio:
 -
 -
@@ -10,7 +15,7 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
 |#
 
 ;;------------ ;;
-;;==== P2 ==== ;;
+;;==== P1 ==== ;;
 ;;------------ ;;
 
 
@@ -19,17 +24,26 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
 ;;----- ;;
 
 
-#|
+#| Gramática BNF
 <prop> ::= (tt)
          | (ff)
-         | ...
+         | (p-id <sym>)
+         | (p-not <prop>)
+         | (p-and <prop> <prop> ...)
+         | (p-or <prop> <prop> ...)
+         | (p-where <prop> <sym> <prop>)
 |#
+;; Prop representa una proposición lógica o booleana, que puede componerse por un booleano simple o por una expresion compuesta de
+;; varios operadores booleanos o definiciones locales.
 
 (deftype Prop
   (tt)
   (ff)
-  ; ...
-  )
+  (p-id x)
+  (p-not b)
+  (p-and props)
+  (p-or props)
+  (p-where b1 id b2))
 
 
 ;;----- ;;
@@ -41,11 +55,33 @@ Concrete syntax of propositions:
 
 <s-prop> ::= true
           | false
-          | ...
+          | <sym>
+          | (list 'not <s-prop>)
+          | (list 'and <s-prop> <s-prop>)
+          | (list 'or <s-prop> <s-prop>)
+          | (list <s-prop> 'where (<sym> <s-prop>))
+
 |#
 
 ;; parse-prop : <s-prop> -> Prop
-(define (parse-prop s-expr) '???)
+;; Parser para las proposiciones, toma el input en formato "normal" para el usuario y lo transforma en un formato que lo pueda leer nuestra estructura
+;; de dato prop
+(define (parse-prop s-expr)
+  (match s-expr
+    ['true (tt)]
+    ['false (ff)]
+    [(? symbol?) (p-id s-expr)]
+    [(list 'not prop) (p-not (parse-prop prop))]
+    [(list 'and x elems ...) 
+     (if (or (null? elems) (null? x))
+         (error 'and "expects at least two operands")
+         (p-and (cons (parse-prop x) (map parse-prop elems))))]
+    [(list 'or x elems ...)
+     (if (or (null? elems) (null? x))
+         (error 'or "expects at least two operands")
+         (p-or (cons (parse-prop x) (map parse-prop elems))))]
+    [(list p 'where (list id expr))
+     (p-where (parse-prop p) id (parse-prop expr))]))
 
 
 ;;----- ;;
@@ -54,10 +90,13 @@ Concrete syntax of propositions:
 
 
 #|
-<value> ::= ...
+<value> ::= (ttV)
+        | (ffV)
 |#
 
-;; (deftype PValue ...)
+(deftype PValue
+  (ttV)
+  (ffV))
 
 ;; from-Pvalue : PValue -> Prop
 (define (from-Pvalue p-value) '???)
@@ -69,7 +108,20 @@ Concrete syntax of propositions:
 
 
 ;; p-subst : Prop Symbol Prop -> Prop
-(define (p-subst target name substitution) '???)
+;; Sustituye un identificador en una proposición por un nuevo valor. Acepta la proposición, el identificador y el nuevo valor, y realiza la
+;; sustitución si hay coincidencias. Si no encuentra coincidencias, devuelve la proposición original.
+(define (p-subst target name substitution)
+  (match target
+    [(p-id x)
+     (if (symbol=? x name)
+         substitution
+         (p-id x))]
+    [(p-where cond id body)
+     (p-where 
+      (p-subst cond name substitution) 
+      id
+      body)]  
+    [_ target]))
 
 
 ;;----- ;;
@@ -96,19 +148,22 @@ Concrete syntax of propositions:
 
 
 #|
-<expr> ::= ...
+<expr> ::= (real <num>)
+        | (imaginary <num>)
         | (add <expr> <expr>)
         | (sub <expr> <expr>)
         | (if0 <expr> <expr> <expr>)
-        | ...
+        | (with <sym> <expr> <expr>)
+        |(id <sym>)
 |#
 (deftype Expr
-  ; ...
+  (real a)
+  (imaginary b)
   (add l r)
   (sub l r)
   (if0 c t f)
-  ; ...
-  )
+  (with x named-expr body)
+  (id x))
 
 ;;----- ;;
 ;; P2.b ;;
@@ -117,16 +172,26 @@ Concrete syntax of propositions:
 #|
 Concrete syntax of expressions:
 
-<s-expr> ::= ...
+<s-expr> ::= <num>
+        | (<num> 'i)
         | (+ <s-expr> <s-expr>)
         | (- <s-expr> <s-expr>)
         | (if0 <s-expr> <s-expr> <s-expr>)
-        | ...
+        | (list 'with [(list <sym> <s-expr>)*] <s-expr>)
+        | <sym>
 |#
 
 ;; parse : <s-expr> -> Expr
-
-(define (parse s-expr) '???)
+;; Parser, se encarga de tomar la expresion en "lenguaje natural" y transformarlo en formato de la estructura de datos Expr definida arriba
+(define (parse s-expr)
+  (match s-expr
+    [(? number? a) (real a)]
+    [(list b 'i) (imaginary b)]
+    [(? symbol? x) (id x)]
+    [(list '+ l-sexpr r-sexpr) (add (parse l-sexpr)(parse r-sexpr))]
+    [(list '- l-sexpr r-sexpr)(sub (parse l-sexpr)(parse r-sexpr))]
+    [(list 'if0 c-sexpr t-sexpr f-sexpr)
+     (if0 (parse c-sexpr) (parse t-sexpr) (parse f-sexpr))]))
 
 ;;----- ;;
 ;; P2.c ;;
--- test.rkt
+++ test.rkt
@@ -2,3 +2,34 @@
 (require "T2.rkt")
 
 (print-only-errors #t)
+
+;;Tests P1.b
+(test (parse-prop ' (not true)) (p-not (tt)))
+(test (parse-prop ' (and true false true)) (p-and ( list (tt) ( ff ) (tt))))
+(test (parse-prop ' (or true false )) (p-or ( list (tt) ( ff ))))
+(test (parse-prop '(false where [x true] )) (p-where (ff) 'x (tt)))
+(test (parse-prop '(x where [x true] )) (p-where (p-id 'x) 'x (tt)))
+
+;;Tests P1.c
+
+
+
+;;Tests P1.d
+(test (p-subst (p-id 'x) 'x (tt) ) (tt))
+(test (p-subst (p-id 'x) 'y (tt)) (p-id 'x))
+(test (p-subst (p-where (p-id 'x) 'x (tt)) 'x (ff)) (p-where (p-id 'x) 'x (tt)))
+(test (p-subst (p-where (p-id 'x) 'y (tt)) 'x (ff)) (p-where (ff) 'y (tt)))
+
+;;Tests P1.e
+
+
+;;Tests P2.a
+(test (parse '1) (real 1))
+(test (parse ' (1 i ))(imaginary 1))
+(test (parse ' (+ 1 (2 i ))) (add (real 1) (imaginary 2)))
+
+
+;;Tests P2.b
+;;Tests P2.c
+;;Tests P2.d
+;;Tests P2.e
